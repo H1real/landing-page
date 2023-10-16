f6c909bf5bf448dd35c5af0eb7fbb8228d7af7fd
H1real <timozil130@gmail.com>
2023-10-16 15:13:37 -0500

    dded indexs.js with node launcher

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+ 
+const server = http.createServer((req, res) => {
+  let html;
+  const url = req.url;
+  if (url === '/') {
+    fs.readFile('./index.html', function (err, html) {
+      if (err) {
+          throw err; 
+      }
+      res.writeHeader(200, {"Content-Type": "text/html"});  
+          res.write(html);  
+          res.end(); 
+    });
+  }
+});
+ 
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
